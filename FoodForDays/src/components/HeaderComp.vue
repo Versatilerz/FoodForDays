598d5d080086bd4d50ff8af3786271d182525b46
Nils van Gestel <nvangestel@yellowtail.nl>
2025-01-10 09:17:42 +0100

    header comp

---
+++ FoodForDays/src/components/HeaderComp.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="header">
+    <nav>Home</nav>
+    <nav>Meals</nav>
+    <nav>Settings</nav>
+    <nav>History</nav>
+    <nav>About</nav>
+  </div>
+</template>
+
+<style scoped>
+.header {
+  background-color: red;
+}
+</style>
--- FoodForDays/src/views/HomeView.vue
+++ FoodForDays/src/views/HomeView.vue
@@ -1,4 +1,11 @@
+<script setup lang="ts">
+import HeaderComp from "@/components/HeaderComp.vue";
+</script>
+
 <template>
+  <head>
+    <HeaderComp />
+  </head>
   <main>
     <h1>test</h1>
   </main>
