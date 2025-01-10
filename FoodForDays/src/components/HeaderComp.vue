367538405b056bb1757de30c657a956c8ef84372
Nils van Gestel <nvangestel@yellowtail.nl>
2025-01-10 11:21:18 +0100

    setting up the views and the navigation

--- FoodForDays/src/components/HeaderComp.vue
+++ FoodForDays/src/components/HeaderComp.vue
@@ -1,15 +1,46 @@
 <template>
   <div class="header">
-    <nav>Home</nav>
-    <nav>Meals</nav>
-    <nav>Settings</nav>
-    <nav>History</nav>
-    <nav>About</nav>
+    <ul class="header__nav">
+      <li>
+        <nav><router-link :to="{ name: 'home' }">Home</router-link></nav>
+      </li>
+      <li>
+        <nav>
+          <nav><router-link :to="{ name: 'meals' }">Meals</router-link></nav>
+        </nav>
+      </li>
+      <li>
+        <nav>
+          <nav><router-link :to="{ name: 'settings' }">Settings</router-link></nav>
+        </nav>
+      </li>
+      <li>
+        <nav>
+          <nav><router-link :to="{ name: 'history' }">History</router-link></nav>
+        </nav>
+      </li>
+      <li>
+        <nav>
+          <nav><router-link :to="{ name: 'about' }">About</router-link></nav>
+        </nav>
+      </li>
+    </ul>
   </div>
 </template>
 
 <style scoped>
 .header {
-  background-color: red;
+  background-color: var(--clr-dark-grey);
+  color: var(--clr-white);
+  display: flex;
+
+  &__nav {
+    list-style: none;
+    display: flex;
+  }
+
+  &__nav > li {
+    padding: 1rem;
+  }
 }
 </style>
--- FoodForDays/src/router/index.ts
+++ FoodForDays/src/router/index.ts
@@ -1,14 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import MealsView from "@/views/MealsView.vue";
+import SettingsView from "@/views/SettingsView.vue";
+import HistoryView from "@/views/HistoryView.vue";
+import AboutView from "@/views/AboutView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomeView,
-    },
+    { path: "/", name: "home", component: HomeView },
+    { path: "/meals", name: "meals", component: MealsView },
+    { path: "/settings", name: "settings", component: SettingsView },
+    { path: "/history", name: "history", component: HistoryView },
+    { path: "/about", name: "about", component: AboutView },
   ],
 });
 
